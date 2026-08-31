aff7f5437c881be6c1a2775223a620374a7d8f0f
BExhei <104512303+BExhei@users.noreply.github.com>
2026-08-31 14:53:42 +0800

    feat: remember window position & size (tauri-plugin-window-state)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -67,6 +67,7 @@ fn get_pending_file(state: tauri::State<'_, AppState>) -> Option<PathBuf> {
 
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
